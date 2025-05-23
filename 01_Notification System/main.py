f9436e9e2e59d53e5b0c6e495d14ab74ecf87b45
Maaz Husyan <mazhusyan120@gmail.com>
2025-05-23 16:07:38 +0500

    change file location

--- 01_Notification System/main.py
+++ 01_Notification System/main.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
-@abstractmethod
 class Notifier(ABC):
+    @abstractmethod
     def send(self, message: str):
         pass
 
@@ -17,15 +17,15 @@ class SimpleMessageService(Notifier):
 
 class Push(Notifier):
     def send(self, message: str):
-        print(f"Sending an Push notification...{message}")
+        print(f"Sending a Push notification...{message}")
         
         
 class NotificationManager:
     def __init__(self, notifier: Notifier) -> None:
-        self.notofier = notifier
+        self.notifier = notifier
         
     def notify(self, message: str):
-        self.notofier.send(message)
+        self.notifier.send(message)
         
 email = Email()
 sms = SimpleMessageService()
