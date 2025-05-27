61080ee2b93152c3c003ec0d01ef2965aa0557cf
Maaz Husyan <mazhusyan120@gmail.com>
2025-05-27 23:15:44 +0500

    create instances and comments

--- 02_Bank Management/main.py
+++ 02_Bank Management/main.py
@@ -1,9 +1,22 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 # class and object 
+
+
+class Account(ABC):
+    @abstractmethod
+    def deposit(self):
+        pass
+    
+    def withdraw(self):
+        pass
+
+
 class Deposit:
     def deposit(self, amount):
         self._balance += amount
         print(f"{amount} is deposited in your bank account.")
+# dep = Deposit()
+
 
 class Withdraw:
     def withdraw(self, amount):
@@ -12,7 +25,9 @@ class Withdraw:
             print(f"{amount} is withdrawn from your bank account.")
         else:
             print("Insufficient funds")
-            
+# khrrrr = Withdraw()
+
+
 class BankAccount(Deposit, Withdraw): #<== Inheritence
     def __init__(self,accouny_holder, balance) -> None:
         self.account_holder = accouny_holder
@@ -20,6 +35,7 @@ class BankAccount(Deposit, Withdraw): #<== Inheritence
         
     def check_balance(self):
         print(f"{self.account_holder}, Your current balance is {self._balance}")
+# alhabib = BankAccount(accouny_holder="maaz", balance=9999999990)
 
 
 class SavingAccount(BankAccount):
@@ -27,23 +43,25 @@ class SavingAccount(BankAccount):
         super().__init__(accouny_holder, balance)
         self.interest_per_rate = interest_per_rate
         self.withdraw_count = 0
-    
+
     def withdraw(self, amount):
         if self.withdraw_count >= 3:
             print(f"Withdraw limit exceeded for this month,")
             return
         super().withdraw(amount)
         self.withdraw_count += 1
-        
+
     def apply_interest(self):
         interest = self._balance /100 * self.interest_per_rate
-        
+
         print(f"%{self.interest_per_rate} interest applied on {self._balance},")
-        
+
         self._balance += interest
-        
+
         print(f"Interest amount: {interest}")
-        
+# save_alh = SavingAccount(accouny_holder="aryan", balance=1, interest_per_rate=1)
+
+
 class CurrentAccount(BankAccount): # <=== Inheritance 
     def __init__(self, accouny_holder, balance, min_balance) -> None:
         super().__init__(accouny_holder, balance)
@@ -54,4 +72,5 @@ class CurrentAccount(BankAccount): # <=== Inheritance
             print(f"Can't withdraw: You must keep minimum balance of {self.min_balance}")
         else:
             super().withdraw(amount)
+# cur_alh = CurrentAccount(accouny_holder="hamza", balance=2, min_balance=1)
 
